94e8363d83f6eab84412affa380ac359ffda9cc4
Annika Zdon <azdon@umich.edu>
2021-01-29 16:29:29 -0500

    edited simple landing page to call spotify auth, added dockerfile

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Testing if this works.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='App'>
+          <a href='http://localhost:8888'> Login to Spotify </a>
+        </div>
       </header>
     </div>
   );
